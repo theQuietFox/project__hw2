b556e418caf3ec075a5a46433125c942f59d1d37
theQuietFox <irina.korehova@gmail.com>
2023-02-24 22:28:06 +0300

    init commit from console

---
+++ src/test/java/ru/qa/irakulikova/ExampleTest.java
@@ -0,0 +1,12 @@
+package ru.qa.irakulikova;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue(2 > 1);
+    }
+ }
