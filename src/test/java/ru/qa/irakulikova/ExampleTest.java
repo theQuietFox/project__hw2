07b57d6df5df44fda766d56a103a884e228eb2b1
theQuietFox <irina.korehova@gmail.com>
2023-02-25 01:03:02 +0300

    change numbers

--- src/test/java/ru/qa/irakulikova/ExampleTest.java
+++ src/test/java/ru/qa/irakulikova/ExampleTest.java
@@ -7,11 +7,11 @@ public class ExampleTest {
 
     @Test
     void exampleTest() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(2 > 7);
     }
     @Test
     void exampleTest() {
-        Assertions.assertTrue(3 > 1);
+        Assertions.assertTrue(3 > 4);
     }
 
  }
